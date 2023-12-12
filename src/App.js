67341d820171c925b740f07c5413d107522a0b5e
Minus0149 <minus.official.01@gmail.com>
2023-12-12 08:39:43 +0530

    Added animations using jQuery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import QuoteBlock from "./Components/QuoteBlock";
 
 function App() {
+	const [color, setColor] = useState("#000");
 	return (
-		<div className="App">
-			<QuoteBlock />
+		<div className="App" style={{ backgroundColor: color }}>
+			<QuoteBlock color={color} setColor={setColor} />
 		</div>
 	);
 }
